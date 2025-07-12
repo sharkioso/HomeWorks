3dbe93444e0f190da018776f441f716d4eb47a98
sharkioso <tetkin04@mail.ru>
2025-07-13 02:42:03 +0500

    weather Consumer create

---
+++ WeatherConsumer/src/main/java/HomeWork/WeatherConsumer/WeatherConsumerApplication.java
@@ -0,0 +1,13 @@
+package HomeWork.WeatherConsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherConsumerApplication.class, args);
+	}
+
+}
---
+++ WeatherConsumer/src/test/java/HomeWork/WeatherConsumer/WeatherConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package HomeWork.WeatherConsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
